d6e1840738ad1459c8e883549638642df2ba74bd
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-02-20 13:24:20 +0530

    Balanced Parenthesis Problem

---
+++ Assignment 4/Balanced_Parenthesis.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isBalancedParenthesis(string exp){
+
+    stack<char> s;
+    char x;
+
+    for(int i = 0; i < exp.length(); i++){
+        if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{'){
+            s.push(exp[i]);
+            continue;
+        }
+
+        if(s.empty()){
+            return false;
+        }
+
+        switch(exp[i]){
+            case ')':
+                x = s.top();
+                s.pop();
+                if(x == '{' || x == '['){
+                    return false;
+                }
+                break;
+            case '}':
+                x = s.top();
+                s.pop();
+                if(x == '(' || x == '[')
+                    return false;
+                break;
+
+            case ']':
+                x = s.top(); 
+                s.pop(); 
+                if (x == '(' || x == '{') 
+                    return false; 
+                break; 
+        }
+    }
+
+    return (s.empty());
+}
+
+int main(){
+
+    string exp = "{()}{}";
+
+    if(isBalancedParenthesis(exp)){
+        cout << "Yes";
+    }else{
+        cout << "No";
+    }
+
+    return 0;
+}
