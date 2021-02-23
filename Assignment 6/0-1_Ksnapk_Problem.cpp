b7d56f88206d66ee36cdfdd9020f395a1fa6e135
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-02-23 12:10:05 +0530

    0-1 Knapsack Problem Solved

---
+++ Assignment 6/0-1_Ksnapk_Problem.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+ 
+// Values -> v
+// Weights -> w
+// Total number of distinct items `n`
+// Knapsack capacity -> W 
+int knapsack(int v[], int w[], int n, int W)
+{
+    if (W < 0) {
+        return INT_MIN;
+    }
+    if (n < 0 || W == 0) {
+        return 0;
+    }
+    int include = v[n] + knapsack(v, w, n - 1, W - w[n]);
+ 
+    int exclude = knapsack(v, w, n - 1, W);
+ 
+    return max (include, exclude);
+}
+ 
+int main()
+{
+    int v[] = { 20, 5, 10, 40, 15, 25 };
+    int w[] = { 1, 2, 3, 8, 7, 4 };
+    int W = 10;
+    int n = sizeof(v) / sizeof(v[0]);
+ 
+    cout << knapsack(v, w, n - 1, W);
+ 
+    return 0;
+}
