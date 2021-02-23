9ba439643ac95ce5dfd9ae0db03dac2ea4c4e877
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-02-23 11:44:37 +0530

    Climbing stairs problem

---
+++ Assignment 6/Climbing_Stairs.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std; 
+
+int fib(int n) 
+{ 
+    if (n <= 1) 
+    {   
+        return n;
+    } 
+    return fib(n - 1) + fib(n - 2); 
+} 
+  
+int countWays(int s) 
+{ 
+    return fib(s + 1); 
+} 
+
+int main() 
+{ 
+    int s = 6; 
+    cout << countWays(s); 
+    return 0; 
+}
